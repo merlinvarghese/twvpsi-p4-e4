7d37647fc8e2b8af8b080bf32bd147c621395024
Vijayalakshmi Velur <velur.viju@gmail.com>
2019-09-04 16:48:58 +0530

    [Nidhi, Vij] Add. Initial Setup

---
+++ src/main/com/tw/vapasi/Parking.java
@@ -0,0 +1,11 @@
+package com.tw.vapasi;
+
+class Parking {
+    String slotNumber;
+    String vehicleNumber;
+    Parking(String slotNumber, String vehicleNumber) {
+        this.slotNumber = slotNumber;
+        this.vehicleNumber = vehicleNumber;
+    }
+}
+
---
+++ src/test/com/tw/vapasi/ParkingTest.java
@@ -0,0 +1,15 @@
+package com.tw.vapasi;
+
+import jdk.jfr.StackTrace;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingTest {
+    @Test
+    void expectEmptySpace() {
+        sl
+        assertTrue(true);
+    }
+
+}
